2494332b0f332f6d358d3801a48d0ab10ba2e512
Muhammad Syahrizal Fauzi <syahrizalfauzi16@gmail.com>
2021-09-18 05:50:45 +0700

    Components done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createContext, useEffect, useReducer, useState } from "react";
+import Selector from "./Components/Selector";
 
-function App() {
+const initialProductState = {
+  products: [],
+  selectedProducts: [],
+  categories: [],
+  selectedCategory: null,
+};
+
+const productReducer = (state, action) => {
+  switch (action.type) {
+    case "setProducts":
+      return { ...state, products: action.payload };
+    case "setCategory":
+      return {
+        ...state,
+        categories: action.payload,
+        selectedCategory: action.payload[0],
+        selectedProducts: state.products.filter(
+          (product) => product.category === action.payload[0]
+        ),
+      };
+    case "changeCategory":
+      return {
+        ...state,
+        selectedProducts: state.products.filter(
+          (product) => product.category === action.payload
+        ),
+        selectedCategory: action.payload,
+      };
+    default:
+      throw new Error();
+  }
+};
+
+export const ProductStateContext = createContext();
+export const ProductDispatchContext = createContext();
+export const FetchingContext = createContext(false);
+
+const App = () => {
+  const [productState, productDispatch] = useReducer(
+    productReducer,
+    initialProductState
+  );
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    setIsFetching(true);
+    fetch("https://fakestoreapi.com/products/")
+      .then((res) => res.json())
+      .then((json) => {
+        productDispatch({ type: "setProducts", payload: json });
+        return fetch("https://fakestoreapi.com/products/categories");
+      })
+      .then((res) => res.json())
+      .then((json) => {
+        productDispatch({ type: "setCategory", payload: json });
+        setIsFetching(false);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductDispatchContext.Provider value={productDispatch}>
+        <ProductStateContext.Provider value={productState}>
+          <FetchingContext.Provider value={isFetching}>
+            <Selector />
+          </FetchingContext.Provider>
+        </ProductStateContext.Provider>
+      </ProductDispatchContext.Provider>
     </div>
   );
-}
+};
 
 export default App;
